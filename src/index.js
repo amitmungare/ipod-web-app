0bb615d5d3094b2d5dd7ba157a1689ee4835a8a3
amitmungare <amungare27@gmail.com>
2022-04-22 16:12:29 +0530

    firebase setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const firebaseConfig = {
+  apiKey: "AIzaSyDJQXnjz-UHsy3WPgu4A7idStkdByCKy-E",
+  authDomain: "ipod-a14b7.firebaseapp.com",
+  projectId: "ipod-a14b7",
+  storageBucket: "ipod-a14b7.appspot.com",
+  messagingSenderId: "767423227046",
+  appId: "1:767423227046:web:f1e17580f5311be5668067",
+  measurementId: "G-Z5EDK7VMJV"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+ReactDOM.render(<App />,document.getElementById('root'));
+
